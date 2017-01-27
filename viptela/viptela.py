26db6229ea6f782fe02f8eb598a22fd37cd8f7b1
Brad Searle <bobthebutcher@users.noreply.github.com>
2017-01-27 11:44:29 +1100

    Issue 1 (#2)
    * seperated out http success/error dicts, created functions for passing http error/success
    
    * added parameters to diable ssl warnings on colsole and add a timeout value
    
    * add timouts to http methods and error handling for login timeout
    
    * add deafult headers to http methods
    
    * add requirements files
    
    * initial commit
    
    * renamed
    
    * adding tests
    
    * add test for http response codes
    
    * add badges

--- viptela/viptela.py
+++ viptela/viptela.py
@@ -1,10 +1,14 @@
 import requests
 
 from collections import namedtuple
-from . exceptions import LoginCredentialsError
+from requests.exceptions import ConnectionError
+from . exceptions import LoginCredentialsError, LoginTimeoutError
 
-HTTP_RESPONSE_CODES = {
+HTTP_SUCCESS_CODES = {
     200: 'Success',
+}
+
+HTTP_ERROR_CODES = {
     400: 'Bad Request',
     403: 'Forbidden',
     404: 'API Not found',
@@ -13,36 +17,28 @@ HTTP_RESPONSE_CODES = {
     500: 'Internal Server Error'
 }
 
+HTTP_RESPONSE_CODES = dict()
+HTTP_RESPONSE_CODES.update(HTTP_SUCCESS_CODES)
+HTTP_RESPONSE_CODES.update(HTTP_ERROR_CODES)
+
+
 # parse_response will return a namedtuple object
 Result = namedtuple('Result', [
     'ok', 'status_code', 'error', 'reason', 'data', 'response'
 ])
 
 
-def parse_response(response):
+def parse_http_success(response):
     """
-    Parse a request response object
+    HTTP 2XX responses
     :param response: requests response object
     :return: namedtuple result object
     """
-    # TODO: This is starting to get messy and needs clean up
-    # Potentially move parsing of HTTP codes to functions
-
-    if response.status_code == 400:
-        json_response = dict()
-        reason = response.json()['error']['details']
-        error = response.json()['error']['message']
-
-    elif not response.status_code == 200:
-        reason, error = HTTP_RESPONSE_CODES[response.status_code]
-
-    else:
-        reason = HTTP_RESPONSE_CODES[response.status_code]
-        error = ''
-
-    if response.request.method in ['GET'] and not response.status_code == 400:
+    if response.request.method in ['GET']:
         try:
             json_response = response.json()['data'] if response.json()['data'] else dict()
+            reason = HTTP_RESPONSE_CODES[response.status_code]
+            error = ''
         except KeyError as e:
             json_response = dict()
             reason = 'No data received from device'
@@ -53,6 +49,8 @@ def parse_response(response):
             error = e
     else:
         json_response = dict()
+        reason = HTTP_RESPONSE_CODES[response.status_code]
+        error = ''
 
     result = Result(
         ok=response.ok,
@@ -65,94 +63,140 @@ def parse_response(response):
     return result
 
 
+def parse_http_error(response):
+    """
+    HTTP 4XX and 5XX responses
+    :param response: requests response object
+    :return: namedtuple result object
+    """
+    json_response = dict()
+    reason = response.json()['error']['details']
+    error = response.json()['error']['message']
+
+    result = Result(
+        ok=response.ok,
+        status_code=response.status_code,
+        reason=reason,
+        error=error,
+        data=json_response,
+        response=response,
+    )
+    return result
+
+
+def parse_response(response):
+    """
+    Parse a request response object
+    :param response: requests response object
+    :return: namedtuple result object
+    """
+    if response.status_code in HTTP_SUCCESS_CODES:
+        return parse_http_success(response)
+
+    elif response.status_code in HTTP_ERROR_CODES:
+        return parse_http_error(response)
+
+
 class Viptela(object):
     """
     Class for use with Viptela vManage API.
     """
     @staticmethod
-    def _get(session, url, headers=None):
+    def _get(session, url, headers=None, timeout=10):
         """
         Perform a HTTP get
         :param session: requests session
         :param url: url to get
         :param headers: HTTP headers
+        :param timeout: Timeout for request response
         :return:
         """
         if headers is None:
             headers = {'Connection': 'keep-alive', 'Content-Type': 'application/json'}
 
-        return parse_response(session.get(url=url, headers=headers))
+        return parse_response(session.get(url=url, headers=headers, timeout=timeout))
 
     @staticmethod
-    def _put(session, url, headers=None, data=None):
+    def _put(session, url, headers=None, data=None, timeout=10):
         """
         Perform a HTTP put
         :param session: requests session
         :param url: url to get
         :param headers: HTTP headers
         :param data: Data payload
+        :param timeout: Timeout for request response
         :return:
         """
         if headers is None:
             # add default headers for put
-            headers = dict()
+            headers = {'Connection': 'keep-alive', 'Content-Type': 'application/json'}
 
         if data is None:
             data = dict()
         pass
 
     @staticmethod
-    def _post(session, url, headers=None, data=None):
+    def _post(session, url, headers=None, data=None, timeout=10):
         """
         Perform a HTTP post
         :param session: requests session
         :param url: url to post
         :param headers: HTTP headers
         :param data: Data payload
+        :param timeout: Timeout for request response
         :return:
         """
         if headers is None:
             # add default headers for post
-            headers = dict()
+            headers = {'Connection': 'keep-alive', 'Content-Type': 'application/json'}
 
         if data is None:
             data = dict()
 
-        return parse_response(session.post(url=url, headers=headers, data=data))
+        return parse_response(session.post(url=url, headers=headers, data=data, timeout=timeout))
 
     @staticmethod
-    def _delete(session, url, headers=None, data=None):
+    def _delete(session, url, headers=None, data=None, timeout=10):
         """
         Perform a HTTP delete
         :param session: requests session
         :param url: url to delete
         :param headers: HTTP headers
         :param data: Data payload
+        :param timeout: Timeout for request response
         :return:
         """
         if headers is None:
             # add default headers for delete
-            headers = dict()
+            headers = {'Connection': 'keep-alive', 'Content-Type': 'application/json'}
 
         if data is None:
             data = dict()
 
         pass
 
-    def __init__(self, user, user_pass, vmanage_server, vmanage_server_port=8443, verify=False):
+    def __init__(self, user, user_pass, vmanage_server, vmanage_server_port=8443,
+                 verify=False, disable_warnings=False, timeout=10):
         """
         Init method for Viptela class
         :param user: API user name
         :param user_pass: API user password
         :param vmanage_server: vManage server IP address or Hostname
         :param vmanage_server_port: vManage API port
-        :param verify: Verify HTTPs certificate
+        :param verify: Verify HTTPs certificate verification
+        :param disable_warnings: Disable console warnings if ssl cert invalid
+        :param timeout: Timeout for request response
         """
         self.user = user
         self.user_pass = user_pass
         self.vmanage_server = vmanage_server
         self.vmanage_server_port = vmanage_server_port
         self.verify = verify
+        self.timeout = timeout
+        self.disable_warnings = disable_warnings
+
+        if self.disable_warnings:
+            requests.packages.urllib3.disable_warnings()
 
         self.base_url = 'https://{0}:{1}/dataservice'.format(
             self.vmanage_server,
@@ -164,12 +208,16 @@ class Viptela(object):
             self.session.verify = self.verify
 
         # login
-        login_result = self._post(
-            session=self.session,
-            url='{0}/j_security_check'.format(self.base_url),
-            headers={'Content-Type': 'application/x-www-form-urlencoded'},
-            data={'j_username': self.user, 'j_password': self.user_pass}
-        )
+        try:
+            login_result = self._post(
+                session=self.session,
+                url='{0}/j_security_check'.format(self.base_url),
+                headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                data={'j_username': self.user, 'j_password': self.user_pass},
+                timeout=self.timeout
+            )
+        except ConnectionError:
+            raise LoginTimeoutError('Could not connect to {0}'.format(self.vmanage_server))
 
         if login_result.response.text.startswith('<html>'):
             raise LoginCredentialsError('Could not login to device, check user credentials')
