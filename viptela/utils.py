62b7b46692efc33a6028d5baa7e903aeca6af6c2
Ben Hohnke <ben.hohnke@nttict.com>
2018-02-28 17:10:05 +1100

    Further refactoring for efficiency

--- viptela/utils.py
+++ viptela/utils.py
@@ -33,17 +33,28 @@ def create_template_dict(template_dir):
     if not template_dir.endswith('/'):
         template_dir += '/'
     template_dict = {}
-    json_files = [f for f in os.listdir(template_dir) if f.endswith('.json')]
+    json_files = [f for f in os.listdir(template_dir) if f.endswith(constants.JSON_EXT)]
     for file in json_files:
         with open(template_dir + file) as f:
-            template_dict[file.split('.json')[0]] = json.loads(f.read())
+            template_dict[file.split(constants.JSON_EXT)[0]] = json.loads(f.read())
     return template_dict
+
+
+def check_post_response(response, raise_error=False):
+    """Check to see if a post response contains an error code or not. If so, the error is logged,
+    and an exception is raised if required."""
+    if not response.error:
+        return response
+    logger.debug('Failure in POST operation: {}'.format(response.reason))
+    if raise_error:
+        raise Exception('Failure in POST operation.')
+    return response
 
 
 def import_provisioning_templates(api_class, template_dict):
     """Import templates from into the vManage server."""
 
-    def check_policy(policy_name):
+    def _check_policy(policy_name):
         return_data = api_class.get(
             api_class.session, api_class.base_url + constants.VEDGE_POLICY_PATH
         ).data
@@ -51,76 +62,76 @@ def import_provisioning_templates(api_class, template_dict):
             if policy[constants.POLICY_NAME] == policy_name:
                 return policy[constants.POLICY_ID]
         return False
+
     device_template_data = template_dict.get('device_template')
+
     for device_template in device_template_data['templates']:
-        template_id_mapping = {}
+        id_map = {}
         feature_data = template_dict.get(
             '{}_features'.format(device_template[constants.TEMPLATE_NAME])
         )
+
         if not feature_data:
             # logger.debug('No feature templates found.')
             continue
+
         for template_id in feature_data:
-            if template_id in feature_data:
-                fd = {
-                    k: v for k, v in feature_data[template_id].items() if k in 
-                    constants.FEATURE_KEYS
-                }
-                feature_template_id_map_data = api_class.get(
-                    api_class.session, api_class.base_url + constants.FEATURE_PATH
-                ).data
-                feature_template_id_map = {
-                    k[constants.TEMPLATE_NAME]: k[constants.TEMPLATE_ID] for k in
-                    feature_template_id_map_data
-                }
-                if fd[constants.TEMPLATE_NAME] in feature_template_id_map:
-                    new_template_id = feature_template_id_map[fd[constants.TEMPLATE_NAME]]
-                else:
-                    post_response = api_class.post(
-                        api_class.session, api_class.base_url + constants.FEATURE_PATH,
-                        data=json.dumps(fd)
-                    )
-                    new_template_id = post_response.data[constants.TEMPLATE_ID]
-                template_id_mapping[template_id] = new_template_id
+            fd = {
+                k: v for k, v in feature_data[template_id].items() if k in
+                constants.FEATURE_KEYS
+            }
+            feature_template_id_map_data = api_class.get(
+                api_class.session, api_class.base_url + constants.FEATURE_PATH
+            ).data
+            feature_template_id_map = {
+                k[constants.TEMPLATE_NAME]: k[constants.TEMPLATE_ID] for k in
+                feature_template_id_map_data
+            }
+
+            template_name = fd[constants.TEMPLATE_NAME]
+            if template_name in feature_template_id_map:
+                new_template_id = feature_template_id_map[template_name]
+            else:
+                post_response = check_post_response(api_class.post(
+                    api_class.session, api_class.base_url + constants.FEATURE_PATH,
+                    data=json.dumps(fd)
+                ))
+                new_template_id = post_response.data[constants.TEMPLATE_ID]
+            id_map[template_id] = new_template_id
+
         new_device_template = copy.deepcopy(device_template)
         for feature in new_device_template[constants.GENERAL_TEMPLATES]:
-            feature[constants.TEMPLATE_ID] = template_id_mapping[feature[constants.TEMPLATE_ID]]
-            if constants.SUB_TEMPLATES in feature:
-                for sub_temp in feature[constants.SUB_TEMPLATES]:
-                    if constants.SUB_TEMPLATES in sub_temp:
-                        for template in sub_temp[constants.SUB_TEMPLATES]:
-                            template[constants.TEMPLATE_ID] = template_id_mapping[
-                                template[constants.TEMPLATE_ID]
-                            ]
-                    sub_temp[constants.TEMPLATE_ID] = template_id_mapping[
-                        sub_temp[constants.TEMPLATE_ID]
-                    ]
-        if constants.UID_RANGE in new_device_template and new_device_template.get(
-                constants.UID_RANGE):
-            for add_template in new_device_template[constants.UID_RANGE]:
-                add_template[constants.TEMPLATE_ID] = template_id_mapping[
-                    add_template[constants.TEMPLATE_ID]
-                ]
-        if constants.POLICY_ID in device_template and device_template[constants.POLICY_ID]:
+            feature[constants.TEMPLATE_ID] = id_map[feature[constants.TEMPLATE_ID]]
+
+            for sub_temp in feature.get(constants.SUB_TEMPLATES, []):
+                for template in sub_temp.get(constants.SUB_TEMPLATES, []):
+                    template[constants.TEMPLATE_ID] = id_map[template[constants.TEMPLATE_ID]]
+                sub_temp[constants.TEMPLATE_ID] = id_map[sub_temp[constants.TEMPLATE_ID]]
+
+        for add_template in new_device_template.get(constants.UID_RANGE, []):
+            add_template[constants.TEMPLATE_ID] = id_map[add_template[constants.TEMPLATE_ID]]
+
+        if device_template.get(constants.POLICY_ID):
             policy_data = template_dict.get(
                 '{}_policy'.format(device_template[constants.TEMPLATE_NAME])
             )
             policy_id = device_template[constants.POLICY_ID]
+
             if policy_id not in policy_data:
                 # logger.debug('Policy data missing in source templates.')
                 pass
             pd = policy_data[policy_id]
-            ch = check_policy(pd[constants.POLICY_NAME])
-            if ch is False:
+            new_policy_id = _check_policy(pd[constants.POLICY_NAME])
+
+            if not new_policy_id:
                 pd = {k: v for k, v in pd.items() if k in constants.POLICY_KEYS}
-                _ = api_class.post(
+                _ = check_post_response(api_class.post(
                     api_class.session, api_class.base_url + constants.VEDGE_POLICY_PATH,
-                    data=json.dumps(pd)
+                    data=json.dumps(pd))
                 )
-                new_policy_id = check_policy(pd[constants.POLICY_NAME])
-            else:
-                new_policy_id = ch
+                new_policy_id = _check_policy(pd[constants.POLICY_NAME])
             new_device_template[constants.POLICY_ID] = new_policy_id
+
         new_device_template = {
             k: v for k, v in new_device_template.items() if k in constants.DEVICE_TEMPLATE_KEYS
         }
@@ -131,12 +142,14 @@ def import_provisioning_templates(api_class, template_dict):
             if device[constants.TEMPLATE_NAME] == new_device_template[constants.TEMPLATE_NAME]:
                 check_device = True
                 break
+
         if not check_device:
-            _ = api_class.post(
+            _ = check_post_response(api_class.post(
                 api_class.session, api_class.base_url + constants.DEVICE_FEATURE_PATH,
                 data=json.dumps(new_device_template)
-            )
+            ))
         else:
             # TODO: Logging here
             # logger.debug('Skipping {}'.format(new_device_template[constants.TEMPLATE_NAME]))
             pass
+    return
